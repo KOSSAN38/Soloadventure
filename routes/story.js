bc76508417ba158af2d716b5d545c48cdee7866e
KOSSAN38 <isak.edlund@elev.ga.ntig.se>
2020-10-23 10:58:16 +0200

    Added and fixed story and links.

--- routes/story.js
+++ routes/story.js
@@ -18,7 +18,7 @@ router.get('/:id', async function (req, res, next) {
             req.params.id
         );
 
-        res.json({
+        res.render('story', {
             id: req.params.id,
             story: story,
             links: links
